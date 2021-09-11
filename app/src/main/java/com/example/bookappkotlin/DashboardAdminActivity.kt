25afb8d5009d0d51d5ad0e838dee31fcdd8b4877
Shihab Ahmed Efty <shihabahmed620@gmail.com>
2021-09-12 00:21:30 +0600

    admin dashboard

--- app/src/main/java/com/example/bookappkotlin/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookappkotlin/DashboardAdminActivity.kt
@@ -1,11 +1,39 @@
 package com.example.bookappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookappkotlin.databinding.ActivityDashboardAdminBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class DashboardAdminActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityDashboardAdminBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_admin)
+        binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+
+        checkUser()
+
+        binding.btnLogout.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+        else{
+            val email = firebaseUser.email
+            binding.tvSubTitle.text = email
+        }
     }
 }
